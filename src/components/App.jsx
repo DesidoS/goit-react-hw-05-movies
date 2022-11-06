107e4063c77651609229de37f51299ade371bb66
Oleksii Kravchenko <djdesidos@gmail.com>
2022-11-06 22:51:15 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
-import { lazy } from 'react';
+// import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout';
 import { Home } from '../pages/Home';
 import { Reviews } from './Reviews';
 import { Cast } from './Cast';
 import { MoviesId } from '../pages/MoviesId';
+import Movies from '../pages/Movies';
 
-const Movies = lazy(() => import('../pages/Movies'));
+// const Movies = lazy(() => import('../pages/Movies'));
 
 export const App = () => {
   return (
